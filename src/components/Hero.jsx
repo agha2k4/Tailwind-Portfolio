6aa82fbac2809186d0bad9dcc6413af6eb788b98
agha47 <agha4ty7@gmail.com>
2024-08-07 15:03:56 +0500

    spelling changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
             <motion.h1 animate={{ x: 100 }}
              className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Kunain Regal</motion.h1>
             <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent '>
-                Full Stack Developer
+                Full Stack Developers
             </span>
             <p className='my-2 max-w-xl py-6 font-Light tracking-tighter'>{HERO_CONTENT}</p>
         </div>
